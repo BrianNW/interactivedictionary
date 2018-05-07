86d8fd6c793394ced0321ef40804f2d9431f92f7
Brian W <Briantechdom@gmail.com>
2018-05-06 19:14:26 -0700

    Added user prompt for similar word suggestor

--- app1.py
+++ app1.py
@@ -12,7 +12,13 @@ def translate(word):
         return data[w]
     # initializes suggestion
     elif len(get_close_matches(w, data.keys())) > 0;
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+        yn input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        if yn == "N":
+            return "The word doesn't exist.  Please double check it."
+        else:
+            return "Input unrecognized. Try again"
     else:
         return "Word doesn't exist.  Please double check it."
 
