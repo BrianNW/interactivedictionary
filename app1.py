96aec504e79e4d0cbec94d26307b4b422a4f1323
Brian W <Briantechdom@gmail.com>
2018-05-06 19:01:48 -0700

    added counting for non-existent data

--- app1.py
+++ app1.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    # counting for nonexistent words
+    if w in data:
+        return data[word]
+    else:
+        return "Word doesn't exist.  Please double check it."
 
 word = input("Enter word: ")
 
